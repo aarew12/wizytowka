98a315be43d3abea85dadf1b1e1eb131b6433c02
aarew12 <44632659+aarew12@users.noreply.github.com>
2019-11-04 00:42:17 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function aboutMe(){
+    document.getElementById("content").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+}
+	
+function offer(){
+    document.getElementById("content").innerHTML = "Nunc finibus ligula ac lorem scelerisque lobortis.";
+}
+
+function portfolio(){
+    document.getElementById("content").innerHTML = "Nulla accumsan dui et enim suscipit, molestie mattis turpis sagittis.";
+}
+
+function hobby(){
+    document.getElementById("content").innerHTML = "Nulla et nisl sit amet ipsum scelerisque vestibulum.";
+}
+
+function contact(){
+    document.getElementById("content").innerHTML = "Nunc eu maximus nulla, vitae scelerisque nunc.";
+}
